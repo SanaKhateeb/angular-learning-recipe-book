e6aa833d8617b8f2881844c9add274990f61e95c
Sana <sanakhateeb14@hotmail.com>
2023-01-17 22:14:58 -0700

    test recipe fake stuff

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Falafel Test', 'mmmm protein and cheap', 'https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-1.jpg')
+  ];
 
 }
