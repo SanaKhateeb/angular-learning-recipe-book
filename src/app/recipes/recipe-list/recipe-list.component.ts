12491129509d83efaab719a14b841749307b6c6a
Sana <sanakhateeb14@hotmail.com>
2023-01-18 19:32:55 -0700

    bruh pls

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe('Falafel Test', 'mmmm protein and cheap', 'https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-1.jpg'),
     new Recipe('Falafel Test', 'mmmm protein and cheap', 'https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-1.jpg')
   ];
 
